d1286d488b7806e5155108134ec26a4c31d5eef0
Dennis Phetsomphou <dennisph@my.yorku.ca>
2019-02-12 21:41:52 -0500

    Added color to the words based on length of response.

--- HTTP_GET/js/helperFunctions.js
+++ HTTP_GET/js/helperFunctions.js
@@ -4,11 +4,13 @@ const renderResponse = (word, res) => {
   if(!res){
     console.log(res.status);
   }
+  /*
   // in case res comes back as a blank array
   if(!res.length){
     responseField.innerHTML = "<p>Try again!</p><p>There were no suggestions found!</p>";
     return;
   }
+  */
 
   // creating an array to contain the HTML strings
   let wordList = [];
@@ -17,12 +19,33 @@ const renderResponse = (word, res) => {
     // creating a list of words
     wordList.push(`<li>${res[i].word}</li>`);
   }
-  // joins the array of HTML strings into one string
-  wordList = wordList.join("");
 
+  console.log(wordList);
+  console.log(wordList.length);
+
+
+/*
   // manipulates responseField to render the modified response
-  //responseField.innerHTML += `<p>You might be interested in:</p><ol>${wordList}</ol>`;
-  responseField.innerHTML += `<div class="dropdown word">${word}<ul class="dropdown-content">${wordList}</ul></div>`
+  if(wordList.length > 5){
+    wordList = wordList.join("");
+    responseField.innerHTML += `<div class="dropdown word green">${word} <ul class="dropdown-content">${wordList}</ul></div>`;
+  } else if(wordList.length > 0){
+    wordList = wordList.join("");
+    responseField.innerHTML += `<div class="dropdown word orange">${word} <ul class="dropdown-content">${wordList}</ul></div>`;
+  } else {
+    responseField.innerHTML += `<div class="dropdown word">${word} <ul class="dropdown-content">${wordList}</ul></div>`;
+  }
+*/
+
+if(wordList.length > 5){
+  wordList = wordList.join("");
+  responseField.innerHTML += `<div class="dropdown word green">${word} <ul class="dropdown-content">${wordList}</ul></div>` + " ";
+} else if(wordList.length > 0){
+  wordList = wordList.join("");
+  responseField.innerHTML += `<div class="dropdown word orange">${word} <ul class="dropdown-content">${wordList}</ul></div>`+ " ";
+} else {
+  responseField.innerHTML += `<div class="dropdown word">${word} <ul class="dropdown-content">${wordList}</ul></div>` + " ";
+}
   return;
 }
 
@@ -32,4 +55,9 @@ const renderRawResponse = (res) => {
   //manipulates responseField to render the unformatted response
   //responseField
   responseField.innerHTML = `<text>${JSON.stringify(trimmedResponse)}</text>`;
+}
+
+function findNonCharacters(word){
+  for(var i = 0; i < word.length; i++){
+  }
 }
