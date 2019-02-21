f924932bf5ac29fe806135267fbdeea6772bd750
Dennis Phetsomphou <dennisph@my.yorku.ca>
2019-02-21 12:14:44 -0500

    (Promises) Maintained output order

--- HTTP_GET/js/helperFunctions.js
+++ HTTP_GET/js/helperFunctions.js
@@ -1,53 +1,39 @@
-// Formats response to look presentable on webpage
-const renderResponse = (word, res) => {
-  // handles if res is falsey
-  if(!res){
-    console.log(res.status);
-  }
-  /*
-  // in case res comes back as a blank array
-  if(!res.length){
-    responseField.innerHTML = "<p>Try again!</p><p>There were no suggestions found!</p>";
-    return;
-  }
-  */
+const render = (word, res) => {
+  return new Promise((resolve, reject) => {
+      // handles if res is falsey
+      if(!res){
+        console.log(res.status);
+      }
+      // creating an array to contain the HTML strings
+      let wordList = [];
+      // looping through the response and maxxing out at 10
+      for(let i = 0; i < Math.min(res.length, 10); i++){
+        // creating a list of words
+        wordList.push(`<li>${res[i].word}</li>`);
+      }
 
-  // creating an array to contain the HTML strings
-  let wordList = [];
-  // looping through the response and maxxing out at 10
-  for(let i = 0; i < Math.min(res.length, 10); i++){
-    // creating a list of words
-    wordList.push(`<li>${res[i].word}</li>`);
-  }
+      console.log(wordList);
+      console.log(wordList.length);
 
-  console.log(wordList);
-  console.log(wordList.length);
-
-
-/*
-  // manipulates responseField to render the modified response
-  if(wordList.length > 5){
-    wordList = wordList.join("");
-    responseField.innerHTML += `<div class="dropdown word green">${word} <ul class="dropdown-content">${wordList}</ul></div>`;
-  } else if(wordList.length > 0){
-    wordList = wordList.join("");
-    responseField.innerHTML += `<div class="dropdown word orange">${word} <ul class="dropdown-content">${wordList}</ul></div>`;
-  } else {
-    responseField.innerHTML += `<div class="dropdown word">${word} <ul class="dropdown-content">${wordList}</ul></div>`;
-  }
-*/
-
-if(wordList.length > 5){
-  wordList = wordList.join("");
-  responseField.innerHTML += `<div class="dropdown word green">${word} <ul class="dropdown-content">${wordList}</ul></div>` + " ";
-} else if(wordList.length > 0){
-  wordList = wordList.join("");
-  responseField.innerHTML += `<div class="dropdown word orange">${word} <ul class="dropdown-content">${wordList}</ul></div>`+ " ";
-} else {
-  responseField.innerHTML += `<div class="dropdown word">${word} <ul class="dropdown-content">${wordList}</ul></div>` + " ";
+      let outputContents = [];
+      if(wordList.length > 5){
+        wordList = wordList.join("");
+        resolve(`<div class="dropdown word green">${word} <ul class="dropdown-content">${wordList}</ul></div>` + " ");
+      } else if(wordList.length > 0){
+        wordList = wordList.join("");
+        resolve(`<div class="dropdown word orange">${word} <ul class="dropdown-content">${wordList}</ul></div>`+ " ");
+      } else {
+        resolve(`<div class="dropdown word">${word} <ul class="dropdown-content">${wordList}</ul></div>` + " ");
+      }
+    })
 }
-  return;
+
+async function toResponseField(){
+  //Waits for promise response
+  let output = await renderResponse();
+  responseField.innerHTML = output;
 }
+
 
 const renderRawResponse = (res) => {
   // taking the first 10 words from res
