eada135eb0aaacf5b2872e17bc9ee9fc4bb8edb9
Dennis Phetsomphou <dennisph@my.yorku.ca>
2019-04-20 16:58:26 -0400

    Cart Toggle

--- React_Projects/shop-site/src/components/cart/cart.jsx
+++ React_Projects/shop-site/src/components/cart/cart.jsx
@@ -2,18 +2,35 @@ import React, { Component } from "react";
 import "./cart.css";
 
 class Cart extends Component {
+  state = {
+    hidden: true
+  };
   render() {
     return (
-      <div className="cart-container">
-        <button onClick={this.toggleDisplay()} className="hidden-display">
-          Shopping Cart {this.props.numOfItems}
-        </button>
+      <div>
+        {this.Display()}
+        <div className="cart-container">
+          <button onClick={this.toggleDisplay} className="hidden-display">
+            Shopping Cart
+          </button>
+        </div>
       </div>
     );
   }
 
-  toggleDisplay() {
-    console.log("Hi");
+  toggleDisplay = () => {
+    console.log(this.state.hidden);
+    this.setState({ hidden: !this.state.hidden });
+  };
+
+  Display() {
+    return this.state.hidden ? null : (
+      <div className="visible-display">
+        <div className="cart-content">
+          <h2>Shopping Cart</h2>
+        </div>
+      </div>
+    );
   }
 }
 
